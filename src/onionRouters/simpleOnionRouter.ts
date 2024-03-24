ba4e5c1a95d717528bfd0c5afe5c3639c0e40842
Lilyy200 <lily.la_morella@edu.devinci.fr>
2024-03-24 15:28:17 +0100

    onion

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,15 +1,44 @@
+import axios from "axios";
+import crypto from "crypto";
 import bodyParser from "body-parser";
-import express from "express";
-import { BASE_ONION_ROUTER_PORT } from "../config";
+import express, { Request, Response } from "express";
+import { BASE_ONION_ROUTER_PORT, REGISTRY_PORT } from "../config";
+import {exportPrvKey, exportPubKey, generateRsaKeyPair, rsaDecrypt, symDecrypt} from "../crypto";
+import {Node} from "../registry/registry";
+import * as console from "console";
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: number | null = null;
+
+  let rsaKeyPair = await generateRsaKeyPair();
+  let pubKey = await exportPubKey(rsaKeyPair.publicKey);
+  let privateKey = rsaKeyPair.privateKey;
+
   // TODO implement the status route
   onionRouter.get("/status", (req, res) => {
     res.send("live");
+  });
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req: Request, res: Response) => {
+    res.status(200).json({ result: lastReceivedEncryptedMessage });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req: Request, res: Response) => {
+    res.status(200).json({ result: lastReceivedDecryptedMessage });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req: Request, res: Response) => {
+    res.status(200).json({ result: lastMessageDestination });
+  });
+
+  onionRouter.get("/getPrivateKey", async (req, res) => {
+    res.status(200).json({result: await exportPrvKey(privateKey)});
   });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
