260549145d840aa5de91a2a9e075b79d552b9428
Lilyy200 <lily.la_morella@edu.devinci.fr>
2024-03-24 15:37:13 +0100

    12

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,11 +1,18 @@
+import axios from "axios";
 import bodyParser from "body-parser";
-import express from "express";
-import { BASE_USER_PORT } from "../config";
+import express, { Request, Response } from "express";
+import { BASE_USER_PORT, REGISTRY_PORT, BASE_ONION_ROUTER_PORT } from "../config";
+import {GetNodeRegistryBody, Node} from "@/src/registry/registry";
+import {createRandomSymmetricKey, exportSymKey, importSymKey, rsaEncrypt, symEncrypt} from "../crypto";
 
 export type SendMessageBody = {
   message: string;
   destinationUserId: number;
 };
+
+let lastCircuit: Node[] = [];
+let lastReceivedMessage: string | null = null;
+let lastSentMessage: string | null = null;
 
 export async function user(userId: number) {
   const _user = express();
@@ -15,6 +22,18 @@ export async function user(userId: number) {
   // TODO implement the status route
   _user.get("/status", (req, res) => {
     res.send("live");
+  });
+
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastSentMessage });
+  });
+
+  _user.get("/getLastCircuit", (req, res) => {
+    res.status(200).json({result: lastCircuit.map((node) => node.nodeId)});
   });
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
